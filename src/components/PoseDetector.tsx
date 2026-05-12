71c9c154bf7d081a56674a17dbcf2c18df89ab8e
C00prum29 <gavrish.mail@gmail.com>
2026-05-12 15:34:56 +0300

    1

--- src/components/PoseDetector.tsx
+++ src/components/PoseDetector.tsx
@@ -198,15 +198,6 @@ export function PoseDetector({ selectedLimb, onAngleUpdate }: PoseDetectorProps)
         });
 
         await camera.start();
-
-        if (video.readyState < 2) {
-          await new Promise((resolve) => {
-            video.onloadedmetadata = () => resolve(true);
-          });
-        }
-
-        await video.play();
-
         cameraRef.current = camera;
         setIsLoading(false);
       } catch (err) {
@@ -244,17 +235,10 @@ export function PoseDetector({ selectedLimb, onAngleUpdate }: PoseDetectorProps)
         )}
         <video
           ref={videoRef}
-          autoPlay
-          muted
+          className="hidden"
           playsInline
-          webkit-playsinline="true"
-          className="absolute opacity-0 pointer-events-none"
-          style={{
-            width: '1px',
-            height: '1px',
-            top: 0,
-            left: 0,
-          }}
+          muted
+          autoPlay
         />
         <canvas
           ref={cameraCanvasRef}
